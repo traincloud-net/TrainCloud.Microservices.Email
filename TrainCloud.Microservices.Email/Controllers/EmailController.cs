4a8a113433c36fce078b4ea3671edf13c5725968
Sebastian Hoyer <65442474+SebHoyer@users.noreply.github.com>
2024-04-25 14:41:59 +0200

    dsfds

--- TrainCloud.Microservices.Email/Controllers/EmailController.cs
+++ TrainCloud.Microservices.Email/Controllers/EmailController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Swashbuckle.AspNetCore.Annotations;
 using TrainCloud.Microservices.Core.Controllers;
+using TrainCloud.Microservices.Core.Services.MessageBus;
 using TrainCloud.Microservices.Email.Models;
 using TrainCloud.Microservices.Email.Services.Email;
 
@@ -12,15 +13,19 @@ namespace TrainCloud.Microservices.Email.Controllers;
 public sealed class EmailController : AbstractController<EmailController>
 {
     private IEmailService EmailService { get; init; }
+    private IMessageBusPublisherService MessageBusPublisherService { get; init; }
+
 
     public EmailController(IWebHostEnvironment webHostEnvironment,
                            IHttpContextAccessor httpContextAccessor,
                            IConfiguration configuration,
                            ILogger<EmailController> logger,
-                           IEmailService emailService)
+                           IEmailService emailService,
+                           IMessageBusPublisherService messageBusPublisherService)
         : base(webHostEnvironment, httpContextAccessor, configuration, logger)
     {
         EmailService = emailService;
+        MessageBusPublisherService = messageBusPublisherService;
     }
 
     [HttpPost("Send")]
@@ -35,6 +40,34 @@ public sealed class EmailController : AbstractController<EmailController>
         try
         {
             await EmailService.SendEmailAsync(new List<string>() { "nico@caratiola.net", "mail@sebastian-hoyer.online" }, null, null, postModel.Subject, postModel.Body, false, null);
+            return Ok();
+        }
+        catch (Exception ex)
+        {
+            return InternalServerError(ex);
+        }
+    }
+
+    [HttpPost("Test")]
+    [Authorize]
+    [SwaggerResponse(StatusCodes.Status200OK)]
+    [SwaggerResponse(StatusCodes.Status400BadRequest)]
+    [SwaggerResponse(StatusCodes.Status401Unauthorized)]
+    [SwaggerResponse(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> PostTestAsync()
+    {
+        try
+        {
+
+            string topicId = Configuration.GetValue<string>("MessageBus:Topics:Email")!;
+            var busMessage = new Email.Messages.SendMailMessage()
+            {
+                To = new List<string> { "mail@sebastian-hoyer.online" },
+                Subject = "TrainCloud email testmail",
+                Body = $"Hallo",
+                IsHtml = true,
+            };
+            await MessageBusPublisherService.SendMessageAsync(topicId, busMessage);
             return Ok();
         }
         catch (Exception ex)
